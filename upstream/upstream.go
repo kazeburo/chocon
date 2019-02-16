17fd59dd57b80eee741cc71d2aa0bb68ec8e622d
Masahiro Nagano <kazeburo@gmail.com>
2019-02-17 01:32:08 +0900

    upstream least_conn

--- upstream/upstream.go
+++ upstream/upstream.go
@@ -2,35 +2,44 @@ package upstream
 
 import (
 	"context"
+	"log"
 	"math/rand"
+	"net"
 	"net/url"
+	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
-	dnscache "go.mercari.io/go-dnscache"
 	"go.uber.org/zap"
 )
 
 // Upstream struct
 type Upstream struct {
-	scheme   string
-	port     string
-	host     string
-	logger   *zap.Logger
-	resolver *dnscache.Resolver
+	scheme string
+	port   string
+	host   string
+	ipwcs  []*IPwc
+	csum   string
+	v      uint64
+	logger *zap.Logger
+	mu     *sync.RWMutex
+}
+
+// IPwc : IP with counter
+type IPwc struct {
+	ip string
+	c  int64
+	v  uint64
 }
 
 // New :
 func New(upstream string, logger *zap.Logger) (*Upstream, error) {
 	var h string
 	var p string
+	var err error
 	u := new(url.URL)
-
-	resolver, err := dnscache.New(3*time.Second, 10*time.Second, logger)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to init dnscache")
-	}
 
 	if upstream != "" {
 		u, err = url.Parse(upstream)
@@ -50,20 +59,29 @@ func New(upstream string, logger *zap.Logger) (*Upstream, error) {
 		if len(hostPortSplit) > 1 {
 			p = hostPortSplit[1]
 		}
-
-		_, err = resolver.Fetch(context.Background(), h)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to resolve upstream")
-		}
 	}
 
-	return &Upstream{
-		scheme:   u.Scheme,
-		host:     h,
-		port:     p,
-		logger:   logger,
-		resolver: resolver,
-	}, nil
+	um := &Upstream{
+		scheme: u.Scheme,
+		host:   h,
+		port:   p,
+		v:      0,
+		logger: logger,
+		mu:     new(sync.RWMutex),
+	}
+
+	if um.Enabled() {
+		ctx := context.Background()
+		ipwcs, err := um.RefreshIP(ctx)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed initial resolv hostname")
+		}
+		if len(ipwcs) < 1 {
+			return nil, errors.New("Could not resolv hostname")
+		}
+		go um.Run(ctx)
+	}
+	return um, nil
 }
 
 // Enabled : upstream is enabled
@@ -76,15 +94,93 @@ func (u *Upstream) GetScheme() string {
 	return u.scheme
 }
 
-// GetHost :
-func (u *Upstream) GetHost(ctx context.Context) (string, error) {
-	ips, err := u.resolver.Fetch(ctx, u.host)
+// RefreshIP : resolve hostname
+func (u *Upstream) RefreshIP(ctx context.Context) ([]*IPwc, error) {
+	u.v++
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	addrs, err := net.DefaultResolver.LookupIPAddr(ctx, u.host)
+	cancel()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	h := ips[rand.Intn(len(ips))].String()
+
+	sort.Slice(addrs, func(i, j int) bool {
+		return addrs[i].IP.String() > addrs[j].IP.String()
+	})
+
+	ips := make([]string, len(addrs))
+	ipwcs := make([]*IPwc, len(addrs))
+	for i, ia := range addrs {
+		ips[i] = ia.IP.String()
+		ipwcs[i] = &IPwc{
+			ip: ia.IP.String(),
+			v:  u.v,
+			c:  0,
+		}
+	}
+	csum := strings.Join(ips, ",")
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	if csum != u.csum {
+		u.csum = csum
+		u.ipwcs = ipwcs
+	}
+
+	return ipwcs, nil
+}
+
+// Run : resolv hostname in background
+func (u *Upstream) Run(ctx context.Context) {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case _ = <-ticker.C:
+			for _, ipwc := range u.ipwcs {
+				log.Printf("%v", ipwc)
+			}
+			_, err := u.RefreshIP(ctx)
+			if err != nil {
+				u.logger.Error("failed refresh ip", zap.Error(err))
+			}
+		}
+	}
+}
+
+// Get : wild
+func (u *Upstream) Get() (string, *IPwc, error) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	sort.Slice(u.ipwcs, func(i, j int) bool {
+		if u.ipwcs[i].c == u.ipwcs[j].c {
+			return rand.Intn(1) == 0
+		}
+		return u.ipwcs[i].c < u.ipwcs[j].c
+	})
+
+	u.ipwcs[0].c++
+	h := u.ipwcs[0].ip
 	if u.port != "" {
 		h = h + ":" + u.port
 	}
-	return h, nil
+	ipwc := &IPwc{
+		ip: u.ipwcs[0].ip,
+		v:  u.ipwcs[0].v,
+		c:  0,
+	}
+	return h, ipwc, nil
+}
+
+// Release : decrement counter
+func (u *Upstream) Release(o *IPwc) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	for i, ipwc := range u.ipwcs {
+		if ipwc.ip == o.ip && ipwc.v == o.v {
+			u.ipwcs[i].c = u.ipwcs[i].c - 1
+		}
+	}
 }
